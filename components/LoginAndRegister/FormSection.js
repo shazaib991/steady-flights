117008dc7b06d8bba1a376ff5cae2a40b7e0ff13
shazaib991 <shazaib424647@gmail.com>
2022-12-05 23:23:35 +0500

    update responsiveness still remaining

--- components/LoginAndRegister/FormSection.js
+++ components/LoginAndRegister/FormSection.js
@@ -6,105 +6,107 @@ export const FormSection = ({ isRegisterPage }) => {
   const router = useRouter();
 
   return (
-    <div className={styles.formSectionWrapper}>
-      <div className={styles.formSectionContainer}>
-        <div
-          className={
-            isRegisterPage
-              ? styles.formSectionRegisterForm
-              : styles.formSectionLoginForm
-          }
-        >
-          <h1>
-            <span className={styles.formSectionContainerSpan1}>
-              {isRegisterPage ? "Register" : "Login"}
-            </span>{" "}
-            {isRegisterPage
-              ? "to fly away with us!"
-              : "and let's explore world!"}
-          </h1>
-          <p className={styles.formSectionFormTitlePara}>
-            {isRegisterPage
-              ? "Flights you need, at prices you'll love."
-              : "At prices you'll love."}
-          </p>
-          <form>
-            <div className={styles.formSectionFormInputContainerEmail}>
-              <p>Email{isRegisterPage && "*"}</p>
-              <input type="text" name="Email" id="Email" />
-            </div>
-            <div className={styles.formSectionFormInputContainerPassword}>
-              <div>
-                <p>Password{isRegisterPage && "*"}</p>
-                <input type="text" name="Password" id="Password" />
+    <div className={styles.formSectionCover}>
+      <div className={styles.formSectionWrapper}>
+        <div className={styles.formSectionContainer}>
+          <div
+            className={
+              isRegisterPage
+                ? styles.formSectionRegisterForm
+                : styles.formSectionLoginForm
+            }
+          >
+            <h1>
+              <span className={styles.formSectionContainerSpan1}>
+                {isRegisterPage ? "Register" : "Login"}
+              </span>{" "}
+              {isRegisterPage
+                ? "to fly away with us!"
+                : "and let's explore world!"}
+            </h1>
+            <p className={styles.formSectionFormTitlePara}>
+              {isRegisterPage
+                ? "Flights you need, at prices you'll love."
+                : "At prices you'll love."}
+            </p>
+            <form>
+              <div className={styles.formSectionFormInputContainerEmail}>
+                <p>Email{isRegisterPage && "*"}</p>
+                <input type="text" name="Email" id="Email" />
               </div>
-              <Image
-                src="/assets/eye 1.svg"
-                alt="hide password"
-                width={24}
-                height={25}
-              />
-            </div>
-          </form>
-          {isRegisterPage ? (
-            <div className={styles.formSectionRegisterSpan}>
-              Password must be at least 8 characters.
-            </div>
-          ) : (
-            <div className={styles.formSectionContainerSpan2}>
-              Reset Password
-            </div>
-          )}
-          {isRegisterPage && (
-            <div>
-              <div
-                className={styles.formSectionNewletterContainer}
-                onClick={(e) => e.currentTarget.classList.toggle(styles.active)}
-              >
-                {/* <Image
-                  src="/assets/Vector.png"
-                  width={16}
-                  height={16}
-                  className={styles.formSectionNewletterCheckboxImage}
-                /> */}
-                <div
-                  className={styles.formSectionNewletterCheckboxUnactive}
-                ></div>
-                <div className={styles.formSectionNewletterCheckbox}>
-                  <div className={styles.formSectionNewletterCheckmark1}></div>
-                  <div className={styles.formSectionNewletterCheckmark2}></div>
+              <div className={styles.formSectionFormInputContainerPassword}>
+                <div>
+                  <p>Password{isRegisterPage && "*"}</p>
+                  <input type="text" name="Password" id="Password" />
                 </div>
-                <p>Subscribe to Newsletter</p>
+                <Image
+                  src="/assets/eye 1.svg"
+                  alt="hide password"
+                  width={20}
+                  height={21}
+                />
               </div>
+            </form>
+            {isRegisterPage ? (
+              <div className={styles.formSectionRegisterSpan}>
+                Password must be at least 8 characters.
+              </div>
+            ) : (
+              <div className={styles.formSectionContainerSpan2}>
+                Reset Password
+              </div>
+            )}
+            {isRegisterPage && (
+              <div>
+                <div
+                  className={styles.formSectionNewletterContainer}
+                  onClick={(e) =>
+                    e.currentTarget.classList.toggle(styles.active)
+                  }
+                >
+                  <div
+                    className={styles.formSectionNewletterCheckboxUnactive}
+                  ></div>
+                  <div className={styles.formSectionNewletterCheckbox}>
+                    <div
+                      className={styles.formSectionNewletterCheckmark1}
+                    ></div>
+                    <div
+                      className={styles.formSectionNewletterCheckmark2}
+                    ></div>
+                  </div>
+                  <p>Subscribe to Newsletter</p>
+                </div>
+              </div>
+            )}
+            <div className={styles.formSectionFormButtonsContainer}>
+              <button className={styles.formSectionFormButtonLogin}>
+                {isRegisterPage ? "Register" : "Login"} with Steady Flights
+              </button>
+              <button className={styles.formSectionFormButtonGoogle}>
+                <Image
+                  src="/assets/logo googleg 48dp.png"
+                  alt="google authorization"
+                  width={24}
+                  height={24}
+                />
+                {isRegisterPage ? "Register" : "Login"} with Google
+              </button>
             </div>
-          )}
-          <div className={styles.formSectionFormButtonsContainer}>
-            <button className={styles.formSectionFormButtonLogin}>
-              {isRegisterPage ? "Register" : "Login"} with Steady Flights
-            </button>
-            <button className={styles.formSectionFormButtonGoogle}>
-              <Image
-                src="/assets/logo googleg 48dp.png"
-                alt="google authorization"
-                width={24}
-                height={24}
-              />
-              {isRegisterPage ? "Register" : "Login"} with Google
-            </button>
+            <p className={styles.formSectionFormRegister}>
+              Don&apos;t have an account?{" "}
+              <span
+                className={styles.formSectionFormSpan3}
+                onClick={
+                  isRegisterPage
+                    ? () => router.replace("/login")
+                    : () => router.replace("/register")
+                }
+              >
+                {isRegisterPage ? "Log in" : "Register"}
+              </span>
+            </p>
           </div>
-          <p className={styles.formSectionFormRegister}>
-            Don&apos;t have an account?{" "}
-            <span
-              className={styles.formSectionFormSpan3}
-              onClick={
-                isRegisterPage
-                  ? () => router.replace("/login")
-                  : () => router.replace("/register")
-              }
-            >
-              {isRegisterPage ? "Log in" : "Register"}
-            </span>
-          </p>
         </div>
       </div>
     </div>
--- components/LoginAndRegister/Header.js
+++ components/LoginAndRegister/Header.js
@@ -3,33 +3,40 @@ import Image from "next/image";
 
 export const Header = () => {
   return (
-    <div className={styles.headerWrapper}>
-      <div className={styles.headerContainer}>
-        <div className={styles.headerAndParaContainer}>
-          <div className={styles.headerLogoContainer}>
+    <div className={styles.headerCover}>
+      <div className={styles.headerWrapper}>
+        <div className={styles.headerContainer}>
+          <div className={styles.headerAndParaContainer}>
+            <div className={styles.headerLogoContainer}>
+              <Image
+                src="/assets/JPG-01.png"
+                alt="steady flights logo"
+                className={styles.headerLogo}
+                width={120}
+                height={39}
+              />
+            </div>
+            <div className={styles.headerBurgerNav}>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className={styles.headerPara}>
+              <p>Fly Away</p>
+              <p>Steady Alerts</p>
+              <p>Steady Rewards</p>
+            </div>
+          </div>
+          <div className={styles.headerContactContainer}>
+            <button className={styles.headerContactButton}>Contact</button>
             <Image
-              src="/assets/JPG-01.png"
-              alt="steady flights logo"
-              className={styles.headerLogo}
-              width={187}
-              height={62}
+              src="/assets/Ellipse 5.png"
+              alt="profile picture"
+              width={36}
+              height={36}
+              className={styles.headerPicture}
             />
           </div>
-          <div className={styles.headerPara}>
-            <p>Fly Away</p>
-            <p>Steady Alerts</p>
-            <p>Steady Rewards</p>
-          </div>
-        </div>
-        <div className={styles.headerContactContainer}>
-          <button className={styles.headerContactButton}>Contact</button>
-          <Image
-            src="/assets/Ellipse 5.png"
-            alt="profile picture"
-            width={44}
-            height={44}
-            className={styles.headerPicture}
-          />
         </div>
       </div>
     </div>
